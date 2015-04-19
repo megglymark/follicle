b8236c69b3153efbaac13f41cc40aba18ab9ffc3
Mark Moniz <megglymark@gmail.com>
2015-04-19 16:48:16 -0700

    Closureizing

--- trnt.go
+++ trnt.go
@@ -13,49 +13,62 @@ type TRNT struct {
 	created_at, updated_at    []byte
 }
 
-func AllTorrents() map[int]*TRNT {
+func Torrents(query string) func() map[int]*TRNT {
 	/*
 	   Open Database
-
 	*/
 	db, err := sql.Open("mysql", "root@/attic")
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
 
 	/*
-			   Query Database
+	   Query Database
 
-			   | id(int) | from(string) | filename(string) | to(string) | size_sent(int) |
-			       | size_total(int) | created_at([]byte) | updated_at([]byte) |
+	   | id(int) | from(string) | filename(string) | to(string) | size_sent(int) |
+	       | size_total(int) | created_at([]byte) | updated_at([]byte) |
 
-		     INFO: 'size_sent' and 'size_total' are set to -1 if they have not been transfered yet
-			   TODO: Change 'created_at' and 'updated_at' type from []byte to time.TIME
+	   INFO: 'size_sent' and 'size_total' are set to -1 if they have not been transfered yet
+	   TODO: Change 'created_at' and 'updated_at' type from []byte to time.TIME
 
 	*/
-	rows, err := db.Query("SELECT * FROM transfers")
+	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//make map of torrents in database
-	m := make(map[int]*TRNT)
-	var filename, from, to string
-	var id, size_sent, size_total int
-	var created_at, updated_at []byte
-	for rows.Next() {
-		if err := rows.Scan(&id, &from, &filename, &to, &size_sent, &size_total, &created_at, &updated_at); err != nil {
-			log.Fatal(err)
+	return func() map[int]*TRNT {
+		/*
+		   Closure
+
+		   INFO: Makes a map of torrents from the provided query
+		   RETURN: map[int]*TNRT
+		*/
+		defer db.Close()
+		defer rows.Close()
+		m := make(map[int]*TRNT)
+		var filename, from, to string
+		var id, size_sent, size_total int
+		var created_at, updated_at []byte
+		for rows.Next() {
+			if err := rows.Scan(&id, &from, &filename, &to, &size_sent, &size_total, &created_at, &updated_at); err != nil {
+				log.Fatal(err)
+			}
+			tor := new(TRNT)
+			tor.WriteTorrent(id, size_sent, size_total, filename, from, to, created_at, updated_at)
+			m[id] = tor
 		}
-		tor := new(TRNT)
-		tor.AppendTorrent(id, size_sent, size_total, filename, from, to, created_at, updated_at)
-		m[id] = tor
+		return m
 	}
-	return m
 }
 
-func (tor *TRNT) AppendTorrent(id, size_sent, size_total int, filename, from, to string, created_at, updated_at []byte) error {
+func (tor *TRNT) WriteTorrent(id, size_sent, size_total int, filename, from, to string, created_at, updated_at []byte) error {
+	/*
+	   Write Torrent
+
+	   INFO: Overwrites values for a given torrent
+	   RETURN: error
+	*/
 	tor.id = id
 	tor.filename = filename
 	tor.from = from
@@ -69,10 +82,13 @@ func (tor *TRNT) AppendTorrent(id, size_sent, size_total int, filename, from, to
 
 func main() {
 
-	m := AllTorrents()
+	//allTorrents := Torrents("SELECT * FROM transfers")
+	//all := allTorrents()
+	unsentTorrents := Torrents("SELECT * FROM transfers WHERE size_sent = -1")
+	unsent := unsentTorrents()
 
 	//print the filename of each row
-	for k, v := range m {
+	for k, v := range unsent {
 		fmt.Println("Key:", k, " Value:", *v)
 	}
 
